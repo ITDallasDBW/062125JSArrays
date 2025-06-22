89dee93056defe38488d7efb833c170cda24373d
Blake <itdallasdbw@gmail.com>
2025-06-22 16:26:15 -0500

    Finished with FILTER. Starting looping thru arrays at 18:00 in video.

---
+++ app.js
@@ -0,0 +1,36 @@
+// let arr = [20, 30, 40, 50, 100]
+
+// //First element of array
+// console.log(ara[0])
+// //Last element of array
+// console.log(ara[ara.length - 1])
+// //Add element to array
+// ara.push(200)
+
+// let newArr = arr.filter((element) => {
+//     console.log(element)
+//     if (element <50) {
+//         return true;
+//     }
+// })
+// console.log(newArr)
+
+// let grades = ['A+', 'A', 'FAIL']
+// let grades = ['FAIL', 'FAIL', 'B']
+// let grades = ['FAIL']
+
+// let newGrades = grades.filter ((grade) => {
+//     console.log(grade)
+//     if (grade != 'FAIL')
+//         return true;
+// })
+// console.log(newGrades)
+
+
+// let grades = ['A+', 'A', 'FAIL']
+
+// let newGrades = grades.filter(grade => grade != 'FAIL')
+// console.log(newGrades)
+
+let grades = ['A+', 'A', 'FAIL']
+
