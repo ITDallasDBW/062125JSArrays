88e859a78c843bb0fb02222150e126958f2687c0
Blake <itdallasdbw@gmail.com>
2025-06-22 19:52:55 -0500

    Arrays Complete

--- app.js
+++ app.js
@@ -32,5 +32,48 @@
 // let newGrades = grades.filter(grade => grade != 'FAIL')
 // console.log(newGrades)
 
-let grades = ['A+', 'A', 'FAIL']
+//Looping through arrays at 18:00
 
+// let grades = ['A+', 'A', 'FAIL']
+// let goodGrades = []
+
+// for (let i = 0; i < grades.length; ++i) {
+// console.log(i);
+// if (grades[i] !== 'FAIL') {
+//     goodGrades.push(grades[i])
+// }
+// }
+// console.log(goodGrades)
+
+//Array.map at 22:45
+// let arr = [1,4,9,16]
+
+// let newArray = arr.map((element) => {
+//     console.log(element)
+//     return undefined;
+// })
+// console.log(newArray)
+
+// let dollars = [1,5,10,3]
+
+// let cents = dollars.map((dollar) => {
+//     console.log(dollar)
+//     return dollar*100;
+// })
+// console.log(cents)
+
+// let dollars = [1,5,10,3]
+
+// let cents = dollars.map(dollar => dollar*100)
+// console.log(cents)
+
+// WITHOUT USING MAP:
+let dollars = [1,5,10,3]
+let cents = []
+
+for (let i=0; i < dollars.length; ++i) {
+    console.log(dollars[i])
+    cents.push(dollars[i]*100);
+}
+console.log(cents);
+//28:38
